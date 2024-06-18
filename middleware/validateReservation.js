15ca40605edf4875bd901410c7a0f72ee5259c2b
marinalmont <150839943+marinalmont@users.noreply.github.com>
2024-06-18 11:55:46 +0200

    Update validateReservation.js

--- middleware/validateReservation.js
+++ middleware/validateReservation.js
@@ -14,4 +14,26 @@ const isValidReservationTime = (date, time) => {
 
 module.exports = isValidReservationTime;
 
+const validateReservationOverlap = async (Reservation, reservationDate, time, duration, numberOfPeople) => {
+    const startTime = new Date(reservationDate);
+    startTime.setHours(time.split(':')[0]);
+    startTime.setMinutes(time.split(':')[1]);
+
+    const endTime = new Date(startTime);
+    endTime.setHours(endTime.getHours() + duration);
+
+    const overlappingReservations = await Reservation.find({
+        date: reservationDate,
+        time: {
+            $gte: startTime,
+            $lt: endTime
+        }
+    });
+
+    const totalPeople = overlappingReservations.reduce((sum, reservation) => sum + reservation.numberOfPeople, 0);
+
+    return (totalPeople + numberOfPeople) <= 30; // Capacidad máxima de 30 comensales
+};
+
+module.exports = validateReservationOverlap;
 
