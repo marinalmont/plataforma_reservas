50ba22f1add78e6cda41b7d1ee1ca2662558a251
marinalmont <150839943+marinalmont@users.noreply.github.com>
2024-06-18 11:53:59 +0200

    Update Reservation.js

--- routes/models/Reservation.js
+++ routes/models/Reservation.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ReservationSchema = new mongoose.Schema({
-    serviceName: { type: String, required: true },
+    Name: { type: String, required: true },
     date: { type: Date, required: true },
     time: { type: String, required: true },
     numberOfPeople: { type: Number, required: true },
